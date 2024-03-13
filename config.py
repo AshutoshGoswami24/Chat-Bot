1db84db3da43cafb314879b0489f310816ed547a
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-13 20:24:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,7 @@
 import re
 import os
 from os import getenv, environ
-from Script import script 
-from dotenv import load_dotenv
-load_dotenv()
-id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
+
       
 
 API_ID = int(environ.get("API_ID", "29917436"))
