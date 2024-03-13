632c2c05b559f78490803ef61e2a7c266e4e201e
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-13 19:12:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-api_id = ""
-api_hash = ""
-bot_token = ""
-ADMINS = ""
+import os
+api_id = os.environ.get("API_ID", "")
+api_hash = os.environ.get("API_HASH", "")
+bot_token = os.environ.get("BOT_TOKEN", "")
+admins = os.environ.get("ADMINS", "")
+
