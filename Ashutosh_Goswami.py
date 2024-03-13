431be19f296236c15c8ac6b78e0a3a6c5581ee68
akpandawep <154840412+akpandawep@users.noreply.github.com>
2024-03-13 20:56:27 +0530

    Update Ashutosh_Goswami.py

--- Ashutosh_Goswami.py
+++ Ashutosh_Goswami.py
@@ -1,5 +1,38 @@
 qa_dict = {
-   "hay": "𝐇𝐚𝐲 {username} 𝐌𝐞𝐫𝐚 𝐁𝐚𝐛𝐮 𝐍𝐞 𝐓𝐡𝐚𝐧𝐚 𝐓𝐡𝐚𝐲𝐚",
+#-BAD-user-#
+     "fuck": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "luda": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "mother fucker": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "sala": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "gandu": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "mc": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "bc": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "Teri maki chut": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "chut": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "maderchod": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "madarchod": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "rundi": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "bhosri": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "bsdk": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "Tera ma ka bhosda": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "Randi": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "Bhonsri Wala": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "Bhonsri Wala": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "Teri maa ka bhosda": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+     "bahanachod": "𝐁𝐡𝐢 𝐌𝐚𝐬𝐢 𝐌𝐚 𝐊𝐨 𝐁𝐨𝐥 𝐃𝐮𝐧𝐠𝐚 𝐓𝐨 𝐆𝐮𝐧𝐝 𝐁𝐚𝐭 𝐁𝐨𝐥𝐭𝐞 𝐇𝐚𝐢 #𝐁𝐥𝐚𝐜𝐤_𝐌𝐚𝐢𝐥😁😁",
+#-noty-ho-reha-baha-ke-luda-#  
+    "xxx": "`XXX=30` in Hind Because 'XXX' Is Roman Ring😁",
+    "xx": "`Xx=20` in Hind Because 'XX' Is Roman Ring😁",
+    "x": "`X=10` in Hind Because 'X' Is Roman Ring😁",
+    "xvidoe": """Play NOW ONLY FOR U [MADARCHOD (B1704 Version)](https://youtu.be/MBW2nPbcDlA?si=eswHz_327kHu7Cmu)""",
+    "xnxx": """Play NOW ONLY FOR U [MADARCHOD (B1704 Version)](https://youtu.be/MBW2nPbcDlA?si=eswHz_327kHu7Cmu)""",
+    "xxxvideo": """Play NOW ONLY FOR U [MADARCHOD (B1704 Version)](https://youtu.be/MBW2nPbcDlA?si=eswHz_327kHu7Cmu)""",
+    "porn": """Play NOW ONLY FOR U [MADARCHOD (B1704 Version)](https://youtu.be/MBW2nPbcDlA?si=eswHz_327kHu7Cmu)""",
+    "sex": """Play NOW ONLY FOR U [MADARCHOD (B1704 Version)](https://youtu.be/MBW2nPbcDlA?si=eswHz_327kHu7Cmu)""",
+    "milf": """Play NOW ONLY FOR U [MADARCHOD (B1704 Version)](https://youtu.be/MBW2nPbcDlA?si=eswHz_327kHu7Cmu)""",
+#-chat-user-#
+
+    "hay": "𝐇𝐚𝐲 {username} 𝐌𝐞𝐫𝐚 𝐁𝐚𝐛𝐮 𝐍𝐞 𝐓𝐡𝐚𝐧𝐚 𝐓𝐡𝐚𝐲𝐚",
     "hello": "𝐇𝐞𝐥𝐥𝐨 {username} 𝐇𝐨𝐰 𝐚𝐫𝐞 𝐲𝐨𝐮?",
     "hii": "𝐡𝐢𝐢 {username} 𝑲𝒚𝒂 𝑯𝒂𝒍 𝑯𝒂𝒊 𝒔𝒖𝒃 𝒎𝒔𝒕 😎",
     "hi": "𝐡𝐢𝐢 {username} 𝑲𝒚𝒂 𝑯𝒂𝒍 𝑯𝒂𝒊 𝒔𝒖𝒃 𝒎𝒔𝒕 😎",
@@ -29,4 +62,109 @@ qa_dict = {
     "awesome": "𝐀𝐚𝐲𝐚",
     "great": "𝐇𝐚𝐚 𝐆𝐫𝐞𝐚𝐭 𝐓𝐨 𝐇𝐚𝐲",
     "nice": "𝐓𝐡𝐚𝐧𝐤",
+
+#-channal-#
+
+    "all": "Plz Send /all",
+    "mov": """
+╭━━━━❰❤️Movie❤️❱━➣
+║
+┣⪼[😎 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 😎](https://t.me/PandaWep)
+║
+┣⪼[🍿 𝗠𝗼𝘃𝗶𝗲 𝗖𝗵𝗮𝗻𝗻𝗮𝗹 🍿](https://t.me/MoviePandaWep)
+║
+┣⪼[🎬 𝗠𝗼𝘃𝗶𝗲 𝗚𝗿𝗼𝘂𝗽 🎬](https://t.me/PandaMovieREQ)
+║
+┣⪼[🦸‍♂️ 𝗠𝗮𝗿𝘃𝗲𝗹 & 𝗗𝗰 𝗔𝗹𝗹 🦸‍♀️](https://t.me/MarvelXDcAll)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "movie": """
+╭━━━━❰❤️Movie❤️❱━➣
+║
+┣⪼[😎 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 😎](https://t.me/PandaWep)
+║
+┣⪼[🍿 𝗠𝗼𝘃𝗶𝗲 𝗖𝗵𝗮𝗻𝗻𝗮𝗹 🍿](https://t.me/MoviePandaWep)
+║
+┣⪼[🎬 𝗠𝗼𝘃𝗶𝗲 𝗚𝗿𝗼𝘂𝗽 🎬](https://t.me/PandaMovieREQ)
+║
+┣⪼[🦸‍♂️ 𝗠𝗮𝗿𝘃𝗲𝗹 & 𝗗𝗰 𝗔𝗹𝗹 🦸‍♀️](https://t.me/MarvelXDcAll)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "web": """
+╭━━━━❰❤️WebSeries❤️❱━➣
+║
+┣⪼[😎 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 😎](https://t.me/PandaWep)
+║
+┣⪼[🎞️ 𝐖𝐞𝐛𝐬𝐞𝐫𝐢𝐞𝐬 🎞️](https://t.me/WebSerisePandaWep)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "web series": """
+╭━━━━❰❤️WebSeries❤️❱━➣
+║
+┣⪼[😎 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 😎](https://t.me/PandaWep)
+║
+┣⪼[🎞️ 𝐖𝐞𝐛𝐬𝐞𝐫𝐢𝐞𝐬 🎞️](https://t.me/WebSerisePandaWep)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "webseries": """
+╭━━━━❰❤️WebSeries❤️❱━➣
+║
+┣⪼[😎 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 😎](https://t.me/PandaWep)
+║
+┣⪼[🎞️ 𝐖𝐞𝐛𝐬𝐞𝐫𝐢𝐞𝐬 🎞️](https://t.me/WebSerisePandaWep)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "wep": """
+╭━━━━❰❤️WebSeries❤️❱━➣
+║
+┣⪼[😎 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 😎](https://t.me/PandaWep)
+║
+┣⪼[🎞️ 𝐖𝐞𝐛𝐬𝐞𝐫𝐢𝐞𝐬 🎞️](https://t.me/WebSerisePandaWep)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "wep series": """
+╭━━━━❰❤️WebSeries❤️❱━➣
+║
+┣⪼[😎 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 😎](https://t.me/PandaWep)
+║
+┣⪼[🎞️ 𝐖𝐞𝐛𝐬𝐞𝐫𝐢𝐞𝐬 🎞️](https://t.me/WebSerisePandaWep)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "wepseries": """
+╭━━━━❰❤️WebSeries❤️❱━➣
+║
+┣⪼[😎 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 😎](https://t.me/PandaWep)
+║
+┣⪼[🎞️ 𝐖𝐞𝐛𝐬𝐞𝐫𝐢𝐞𝐬 🎞️](https://t.me/WebSerisePandaWep)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "bot": 
+    """
+╭━━━━❰❤️PandaWepAll❤️❱━➣
+║   
+┣⪼[⭐️ 𝗙𝗶𝗹𝗲 𝗧𝗼 𝗨𝗿𝗹 𝗕𝗼𝘁⭐️](https://t.me/FileToUrlX_Bot)
+║   
+┣⪼[🏧𝗔𝘂𝘁𝗼 𝗥𝗲𝗻𝗮𝗺𝗲 𝗕𝗼𝘁🏧](https://t.me/AutoRenamePro_bot)
+║   
+┣⪼[🔽 𝗔𝗹𝗹 𝗦𝗮𝘃𝗲 𝗕𝗼𝘁🔽](https://t.me/AllSaveBot_bot)
+║
+┣⪼[📝 𝗙𝗶𝗹𝗲 𝗥𝗲𝗻𝗮𝗺𝗲 𝗕𝗼𝘁📝](https://t.me/FileRenameXBot_bot)
+║
+┣⪼[🎬 𝗣𝗮𝗻𝗱𝗮𝗙𝗶𝗹𝘁𝗲𝗿 𝗕𝗼𝘁🎬](https://t.me/PandaFilter_bot)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "anime": """
+╭━━━━❰❤️Anime In Hindi❤️❱━➣
+║   
+┣⪼[🉐 𝗛𝗶𝗻𝗱𝗶 𝗔𝗻𝗶𝗺𝗲 🉐](https://t.me/AnimePandaWep)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "anima": """
+╭━━━━❰❤️Anime In Hindi❤️❱━➣
+║   
+┣⪼[🉐 𝗛𝗶𝗻𝗱𝗶 𝗔𝗻𝗶𝗺𝗲 🉐](https://t.me/AnimePandaWep)
+║
+╰━━━━━━━━━━━━━━━➣""",
+    "": "",
+    
 }
