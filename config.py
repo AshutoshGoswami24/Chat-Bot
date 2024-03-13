e4b79395a4bb96f2803d528bd8f0c0a1e1ccde3a
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-13 20:12:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,22 @@
+import re
 import os
-api_id = os.environ.get("API_ID", "29917436")
-api_hash = os.environ.get("API_HASH", "4a926822b076a086a167fe8f2701d3e9")
-bot_token = os.environ.get("BOT_TOKEN", "7000509878:AAGseZORtAvEKwuHiESBV1u6E0oCkVq8RFQ")
-admins = os.environ.get("ADMINS", "6141937812")
+from os import getenv, environ
+from Script import script 
+from dotenv import load_dotenv
+load_dotenv()
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+      
+
+API_ID = int(environ.get("API_ID", "29917436"))
+API_HASH = environ.get("API_HASH", "4a926822b076a086a167fe8f2701d3e9")
+ADMINS = int(environ.get("ADMINS", "6141937812"))
+BOT_TOKEN = environ.get("BOT_TOKEN", "7000509878:AAGseZORtAvEKwuHiESBV1u6E0oCkVq8RFQ")
 PICS = (environ.get('PICS', 'https://graph.org/file/7bf517f440a124dc68d80.jpg https://graph.org/file/c42f1ed5e9e9f134b9ddc.jpg https://graph.org/file/a24f0b32ead8fde3adc21.jpg https://graph.org/file/a89bd2adf24a1b1f77bab.jpg https://graph.org/file/80f5cbdf1a7c47e77b490.jpg')).split()
 
