4bd4831ea4ab2ea300d3e0eee0f9d6e42f3971c0
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-13 19:34:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-api_id = os.environ.get("API_ID", "29917436")
-api_hash = os.environ.get("API_HASH", "4a926822b076a086a167fe8f2701d3e9")
-bot_token = os.environ.get("BOT_TOKEN", "7000509878:AAGseZORtAvEKwuHiESBV1u6E0oCkVq8RFQ")
-admins = os.environ.get("ADMINS", "6141937812")
-pics = os.environ.get("PICS", "https://graph.org/file/7bf517f440a124dc68d80.jpg https://graph.org/file/c42f1ed5e9e9f134b9ddc.jpg https://graph.org/file/a24f0b32ead8fde3adc21.jpg https://graph.org/file/a89bd2adf24a1b1f77bab.jpg https://graph.org/file/80f5cbdf1a7c47e77b490.jpg")
+api_id = os.environ.get("API_ID", "")
+api_hash = os.environ.get("API_HASH", "")
+bot_token = os.environ.get("BOT_TOKEN", "")
+admins = os.environ.get("ADMINS", "")
+pics = os.environ.get("PICS", "")
