b1bb4e3d37b521e72eeabeb06b5291cc9eeb39a3
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-13 20:48:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import re
 import os
 from os import getenv, environ
-
-      
-
 api_id = int(environ.get("API_ID", ""))
 api_hash = environ.get("API_HASH", "")
 ADMINS = int(environ.get("ADMINS", ""))
