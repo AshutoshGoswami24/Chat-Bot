1ac8a869cfe43f1302ab3158dd8e97296684476d
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-14 06:25:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ api_hash = environ.get("API_HASH", "")
 ADMINS = int(environ.get("ADMINS", ""))
 bot_token = environ.get("BOT_TOKEN", "")
 PICS = (environ.get('PICS', '')).split()
-
+OPENAI_API_KEY = environ.get("OPENAI_API_KEY", "")
