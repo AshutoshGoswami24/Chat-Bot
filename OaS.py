ae605383beec8e9edea54ee07c7577e8712ed92e
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-15 18:14:38 +0530

    Update OaS.py

--- OaS.py
+++ OaS.py
@@ -1,35 +1,32 @@
 from config import *
 from pyrogram import Client, filters
 
-@app.on_message(filters.command("block", prefixes="/") & filters.private)
-async def block_command(client, message):
-    # Block the user who sent the command
-    await client.block_user(message.from_user.id)
-    await message.reply("You have been blocked.")
+async def send_message_to_admins(message):
+    for admin_id in admins:
+        await app.send_message(admin_id, message)
+        
+# Define a handler for the /start command
+@app.on_message(filters.private & filters.command(["st"]))
+async def start_command(client, message):
+    me = await app.get_me()
+    await message.reply("Welcome to the bot!")
 
-# Define a handler for the /unblock command
-@app.on_message(filters.command("unblock", prefixes="/") & filters.private)
-async def unblock_command(client, message):
-    # Check if user is an admin
+    # Check if the user is an admin
     if message.from_user.id in admins:
-        # Unblock the user specified in the command
+        # Get the user ID from the command
         if len(message.command) > 1:
-            try:
-                user_id = int(message.command[1])
-                await client.unblock_user(user_id)
-                await message.reply("User has been unblocked.")
-            except ValueError:
-                await message.reply("Invalid user ID.")
+            user_id = int(message.command[1])
+
+            # Block the user
+            await app.block_user(user_id)
+            await message.reply(f"User {user_id} has been blocked.")
+
+            # Notify admins about the action
+            await send_message_to_admins(f"Admin {message.from_user.id} has blocked user {user_id}.")
         else:
-            await message.reply("Please specify the user's ID to unblock.")
+            await message.reply("Please specify the user's ID to block.")
     else:
         await message.reply("You are not authorized to use this command.")
 
-# Define a handler for all messages
-@app.on_message(filters.private)
-async def handle_messages(client, message):
-    # Check if the message is "/block"
-    if message.text.lower() == "/block":
-        # Block the user who sent the message
-        await client.block_user(message.from_user.id)
-        await message.reply("You have been blocked.")
+# Start the Pyrogram client
+app.run()
