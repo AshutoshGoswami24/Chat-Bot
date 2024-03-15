8327238e00f860e54a66c62a105e0105b74bdad5
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-15 19:49:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,70 @@ print("𝐈𝐧𝐬𝐭𝐚𝐥𝐥𝐢𝐧𝐠😴......")
 welcome_message = "{username} 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗧𝗼 𝗧𝗵𝗲 𝗙𝗮𝗺𝗶𝗹𝘆  😁"
 goodbye_message = "𝗚𝗼𝗼𝗱 𝗕𝘆𝗲 {username} 🥺 𝘄𝗲 𝘄𝗶𝗹𝗹 𝗺𝗶𝘀𝘀 𝘆𝗼𝘂"
 
-#Dont-cng-in-this
+START_TXT = """<b>𝐇𝐞𝐥𝐥𝐨 {}, ɪ ᴀᴍ {},ɪ ᴀᴍ ᴀ ᴄʜᴀᴛʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴘᴀɴᴅᴀᴡᴇʙ. ᴀᴅᴅ ᴍᴇ ᴛᴏ ᴀɴʏ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ᴍᴇ ᴀɴ ᴀᴅᴍɪɴ, ᴛʜᴇɴ ᴄʜᴀᴛ ᴡɪᴛʜ ᴍᴇ ᴀs ᴀ ғʀɪᴇɴᴅ. 😊 [𝙈𝙮 𝘾𝙝𝙖𝙣𝙣𝙖𝙡](https://t.me/Pandawep)</b>"""
+ALL_TXT = """<b>𝐇𝐞𝐥𝐥𝐨 {}, 𝐓𝐡𝐢𝐬 𝐢𝐬 𝐦𝐲 𝐚𝐥𝐥 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 𝐚𝐧𝐝 𝐛𝐨𝐭𝐬. 🤖</b>"""
+
+@app.on_message(filters.command("start") & filters.incoming)
+async def start(client, message):
+    buttons = [[
+        InlineKeyboardButton('😎Main Channal 😎', url='https://t.me/pandawep')
+    ],[
+        InlineKeyboardButton('❤️ Chat Family ❤️', url='https://t.me/PandaWepChat')
+        ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    me2 = (await client.get_me()).mention
+    await message.reply_photo(
+        photo=random.choice(PICS),
+        caption=START_TXT.format(message.from_user.mention, me2),
+        reply_markup=reply_markup
+    )
+
+print("START MASSAGE BUTTON OR FUNCTION Check🟢......")
+# Handler for /all command
+@app.on_message(filters.command("all") & filters.incoming)
+async def all_command(client, message):
+    buttons = [
+        [
+            InlineKeyboardButton('😎Main Channal 😎', url='https://t.me/pandawep')
+        ],
+        [
+            InlineKeyboardButton('⭐️ File To Url Bot ⭐️', url='https://t.me/FileToUrlX_Bot'),
+            InlineKeyboardButton('🏧 Auto Rename Bot 🏧', url='https://t.me/AutoRenamePro_bot')
+        ],
+        [
+            InlineKeyboardButton('🔽 All Save Bot 🔽', url='https://t.me/AllSaveBot_bot'),
+            InlineKeyboardButton('📝 File Rename Bot 📝', url='https://t.me/FileRenameXBot_bot')
+        ],
+        [
+            InlineKeyboardButton('🍿 Movie Channel 🍿', url='https://t.me/MoviePandaWep'),
+            InlineKeyboardButton('🎬 Movie Group 🎬', url='https://t.me/PandaMovieREQ')
+        ],
+        [
+            InlineKeyboardButton('🉐 Hindi Anime 🉐', url='https://t.me/AnimePandaWep'),
+            InlineKeyboardButton('👶 Cartoon 👶', url='https://t.me/CTPandaWep')
+        ],
+        [
+            InlineKeyboardButton('🎞️ Webseries 🎞️', url='https://t.me/WebSerisePandaWep'),
+            InlineKeyboardButton('📚 Books And Free Course 📚', url='https://t.me/BooksPandaWep')
+        ],
+        [
+            InlineKeyboardButton('📱 Mod Apps 📱', url='https://t.me/ApkAshuModKing24'),
+            InlineKeyboardButton('🦸‍♂️ Marvel & DC All 🦸‍♀️', url='https://t.me/MarvelXDcAll')
+        ],
+        [
+            InlineKeyboardButton('❤️ Chat Family ❤️', url='https://t.me/PandaWepChat'),
+            InlineKeyboardButton('🎬 PandaFilter Bot 🎬', url='https://t.me/PandaFilter_bot')
+        ] 
+        ]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    me2 = (await client.get_me()).mention
+    await message.reply_photo(
+        photo=random.choice(PICS),
+        caption=ALL_TXT.format(message.from_user.mention, me2),
+        reply_markup=reply_markup)
+
+
 
 print("WELCOME AND GOOD BYE MSG Check🟢......")
 # Function to handle messages in groups or channels
