a89419d0c7ac28886c61820746ce011d29913b82
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-17 20:16:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 from os import getenv, environ
 api_id = int(environ.get("API_ID", ""))
 api_hash = environ.get("API_HASH", "")
-ADMINS = int(environ.get("ADMINS", "6141937812"))
+ADMINS = int(environ.get("ADMINS", ""))
 bot_token = environ.get("BOT_TOKEN", "")
-PICS = (environ.get('PICS', 'https://graph.org/file/7bf517f440a124dc68d80.jpg https://graph.org/file/c42f1ed5e9e9f134b9ddc.jpg https://graph.org/file/a24f0b32ead8fde3adc21.jpg https://graph.org/file/a89bd2adf24a1b1f77bab.jpg https://graph.org/file/80f5cbdf1a7c47e77b490.jpg')).split()
+PICS = (environ.get('PICS', '')).split()
