9522ac8bce6c1f8f00dce7e3bad4816ca59f8756
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-04-04 04:31:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ api_hash = environ.get("API_HASH", "")
 ADMINS = int(environ.get("ADMINS", ""))
 bot_token = environ.get("BOT_TOKEN", "")
 PICS = (environ.get('PICS', '')).split()
-I-Am-Ashu = (environ.get('I-Am-Ashu', 'True')).split()
+I-Am-Ashu = True
 START_TXT = """<b>𝐇𝐞𝐥𝐥𝐨 {}, ɪ ᴀᴍ {},ɪ ᴀᴍ ᴀ ᴄʜᴀᴛʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴘᴀɴᴅᴀᴡᴇʙ. ᴀᴅᴅ ᴍᴇ ᴛᴏ ᴀɴʏ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ᴍᴇ ᴀɴ ᴀᴅᴍɪɴ, ᴛʜᴇɴ ᴄʜᴀᴛ ᴡɪᴛʜ ᴍᴇ ᴀs ᴀ ғʀɪᴇɴᴅ. 😊 [𝙈𝙮 𝘾𝙝𝙖𝙣𝙣𝙖𝙡](https://t.me/Pandawep)</b>"""
 ALL_TXT = """<b>𝐇𝐞𝐥𝐥𝐨 {}, 𝐓𝐡𝐢𝐬 𝐢𝐬 𝐦𝐲 𝐚𝐥𝐥 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 𝐚𝐧𝐝 𝐛𝐨𝐭𝐬. 🤖</b>"""
