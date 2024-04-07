7d5219b0a2146b4d4e3d0d5fecc7ef1d1502f802
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-04-07 23:34:39 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -24,52 +24,6 @@ print("𝐈𝐧𝐬𝐭𝐚𝐥𝐥𝐢𝐧𝐠😴......")
 welcome_message = "{username} 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗧𝗼 𝗧𝗵𝗲 𝗙𝗮𝗺𝗶𝗹𝘆  😁"
 goodbye_message = "𝗚𝗼𝗼𝗱 𝗕𝘆𝗲 {username} 🥺 𝘄𝗲 𝘄𝗶𝗹𝗹 𝗺𝗶𝘀𝘀 𝘆𝗼𝘂"
 
-@app.on_message(filters.group & filters.new_chat_members)
-async def handle_new_chat_members(client, message):
-    for new_member in message.new_chat_members:
-        username = new_member.username
-        if username:
-            response = welcome_message.format(username="@" + username)
-            await message.reply_text(response)
-        else:
-            response = welcome_message.format(username="this new member")
-            await message.reply_text(response)
-
-
-#main Cod
-
-
-# Function to handle messages in groups or channels
-@app.on_message(filters.group & (filters.text | filters.command))
-async def handle_message(client, message):
-    text = message.text.lower()
-
-    # Send the response from qa_dict as a new message
-    if text in qa_dict:
-        response = qa_dict[text]
-        # Replace {username} with the actual username if available
-        if "{username}" in response:
-            if message.from_user.username:
-                username = "@" + message.from_user.username
-                response = response.replace("{username}", username)
-            else:
-                response = response.replace("{username}", "this user")
-        await message.reply(response)
-
-print("QRD TXT AND FUNCTION Check ALL OF AND NEX......") 
-
-
-
-
-@app.on_message(filters.group & filters.left_chat_member)
-async def handle_left_chat_member(client, message):
-    user_info = message.left_chat_member
-    if user_info.username:
-        response = goodbye_message.format(username="@" + user_info.username)
-        await message.reply_text(response)
-    else:
-        response = goodbye_message.format(username="this user")
-        await message.reply_text(response)
 
 print("𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝😁......")
 
